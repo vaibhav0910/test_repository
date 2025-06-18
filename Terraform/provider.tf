0b00f567865f78469c0c57e04a21de1fcdcf0d56
vaibhav <vaibhav.vm.mittal@gmail.com>
2025-06-19 00:40:54 +0530

    test

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -5,7 +5,7 @@ terraform {
  #   tenant_id            = "00000000-0000-0000-0000-000000000000"  # Can also be set via `ARM_TENANT_ID` environment variable.
  #   client_id            = "00000000-0000-0000-0000-000000000000"  # Can also be set via `ARM_CLIENT_ID` environment variable. Not required for System Assigned Managed Identity.
     resource_group_name = "vaibhav_rg"
-    storage_account_name = "vaibhavstorage12"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    storage_account_name = "vaibhavstorage123"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "vaibhavcontainer"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "vaibhav.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
